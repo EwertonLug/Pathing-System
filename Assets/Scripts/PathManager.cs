409048cf4c3c148d1003df8bae4a1592a9649741
EwertonLug <41789334+EwertonLug@users.noreply.github.com>
2019-11-22 20:12:52 -0400

    221120192212

--- Assets/Scripts/PathManager.cs
+++ Assets/Scripts/PathManager.cs
@@ -3,6 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 //http://www.trickyfast.com/2017/09/21/building-a-waypoint-pathing-system-in-unity/
 //https://gamedev.stackexchange.com/questions/118912/how-can-i-adapt-a-pathfinding-to-work-with-platformers
+public enum IAState
+{
+    JUMP,
+    DROP,
+    WALK,
+    IDLE
+}
+
+
 public class PathManager : MonoBehaviour
 {
     public float walkSpeed = 5.0f;
@@ -14,10 +23,13 @@ public class PathManager : MonoBehaviour
     private float moveTimeCurrent;
     public Transform target;
 
-
+    public int frequencyPerWayPoints;
+    int currentFrequency = 0;
+    public IAState STATE;
     void Start()
     {
-        InvokeRepeating("Search", 2, 5f);
+        //InvokeRepeating("Search", 2, 5f);
+        Search();
     }
     void Search()
     {
@@ -28,7 +40,7 @@ public class PathManager : MonoBehaviour
     public void NavigateTo(Vector3 destination)
     {
         currentPath = new Stack<Vector3>();
-        var currentNode = FindClosestWaypoint(transform.position);
+        var currentNode = FindClosestWaypoint(transform.parent.position);
         var endNode = FindClosestWaypoint(destination);
         if (currentNode == null || endNode == null || currentNode == endNode)
             return;
@@ -50,14 +62,14 @@ public class PathManager : MonoBehaviour
             foreach (var neighbor in currentNode.neighbors)
             {
 
-                if (closedList.Contains(neighbor) || openList.ContainsValue(neighbor) )
+                if (closedList.Contains(neighbor) || openList.ContainsValue(neighbor))
                     continue;
 
-			    if(neighbor == null){continue;}
+                if (neighbor == null) { continue; }
                 neighbor.previous = currentNode;
                 neighbor.distance = dist + (neighbor.transform.position - currentNode.transform.position).magnitude;
                 var distanceToTarget = (neighbor.transform.position - endNode.transform.position).magnitude;
-            
+
 
 
                 if (!openList.ContainsKey(neighbor.distance + distanceToTarget))
@@ -75,7 +87,7 @@ public class PathManager : MonoBehaviour
                 currentPath.Push(currentNode.transform.position);
                 currentNode = currentNode.previous;
             }
-            currentPath.Push(transform.position);
+            currentPath.Push(transform.parent.position);
         }
     }
 
@@ -84,14 +96,12 @@ public class PathManager : MonoBehaviour
         currentPath = null;
         moveTimeTotal = 0;
         moveTimeCurrent = 0;
+
     }
 
     void Update()
     {
-        if (Input.GetKey(KeyCode.Space))
-        {
-            Search();
-        }
+
         if (currentPath != null && currentPath.Count > 0)
         {
             nextWayPoint = currentPath.Peek();
@@ -106,50 +116,113 @@ public class PathManager : MonoBehaviour
             {
                 currentWaypointPosition = currentPath.Pop();
                 if (currentPath.Count == 0)
+                {
                     Stop();
+                  
+                }
                 else
                 {
                     moveTimeCurrent = 0;
+                    currentFrequency++;
                     moveTimeTotal = (currentWaypointPosition - currentPath.Peek()).magnitude / walkSpeed;
+
                 }
             }
         }
-        if(nextWayPoint.y > currentWaypointPosition.y+0.5f){
-          OnJump();
-        }else{
-          OnDwon();
+        else
+        {
+            currentFrequency = 3;
+
         }
+        if (nextWayPoint.y > currentWaypointPosition.y + 0.5f)
+        {
+            OnJump();
+        }
+        else if (nextWayPoint.y < currentWaypointPosition.y - 0.5f)
+        {
+            OnDrop();
+        }
+        else if (nextWayPoint == currentWaypointPosition){
+           
+            OnIdle();
+            
+           
+
+        }else{
+           
+            STATE = IAState.WALK;
+        }
+        //
+         if (nextWayPoint.x > currentWaypointPosition.x + 0.5f)
+        {
+            GetComponent<SpriteRenderer>().flipX = false;
+        }
+        else if (nextWayPoint.x<currentWaypointPosition.x - 0.5f)
+        {
+            GetComponent<SpriteRenderer>().flipX = true;
+        }
+        if (currentFrequency > frequencyPerWayPoints)
+        {
+            Search();
+currentFrequency = 0;
+        }
+        switch (STATE)
+        {
+            case IAState.WALK:
+             GetComponent<Animator>().Play("enemy-walk");
+            break;
+            case IAState.JUMP:
+             GetComponent<Animator>().Play("enemy-jump-in");
+            break;
+            case IAState.DROP:
+             GetComponent<Animator>().Play("enemy-jump-out");
+            break;
+             case IAState.IDLE:
+             GetComponent<Animator>().Play("enemy-idle");
+            break;
+           
+        }
+
     }
 
     private WayPoint FindClosestWaypoint(Vector3 target)
+{
+    GameObject closest = null;
+    float closestDist = Mathf.Infinity;
+    foreach (var waypoint in GameObject.FindGameObjectsWithTag("Waypoint"))
     {
-        GameObject closest = null;
-        float closestDist = Mathf.Infinity;
-        foreach (var waypoint in GameObject.FindGameObjectsWithTag("Waypoint"))
+        var dist = (waypoint.transform.position - target).magnitude;
+        if (dist < closestDist)
         {
-            var dist = (waypoint.transform.position - target).magnitude;
-            if (dist < closestDist)
-            {
-                closest = waypoint;
-                closestDist = dist;
-            }
+            closest = waypoint;
+            closestDist = dist;
         }
-        if (closest != null)
-        {
-            return closest.GetComponent<WayPoint>();
-        }
-        return null;
     }
-    public void OnMove(){
-          transform.position = Vector3.Lerp(currentWaypointPosition, currentPath.Peek(), moveTimeCurrent / moveTimeTotal);
+    if (closest != null)
+    {
+        return closest.GetComponent<WayPoint>();
     }
-    public void  OnJump(){
-         Debug.Log("Pulcar");
-        transform.GetChild(0).GetComponent<SpriteRenderer>().flipY = true;
-    }
-    public void OnDwon(){
-        Debug.Log("Descer");
-        transform.GetChild(0).GetComponent<SpriteRenderer>().flipY = false;
-    }
+    return null;
+}
+public void OnMove()
+{
+
+    transform.parent.position = Vector3.Lerp(currentWaypointPosition, currentPath.Peek(), moveTimeCurrent / moveTimeTotal);
+}
+public void OnJump()
+{
+    STATE = IAState.JUMP;
+
+
+}
+public void OnDrop()
+{
+    STATE = IAState.DROP;
+
+}
+public void OnIdle()
+{
+    STATE = IAState.IDLE;
+}
 
 }
