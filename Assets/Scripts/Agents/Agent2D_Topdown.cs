c604c71993c8783e80c5efa3b78da9e73f7a5bd7
Ewerton <ewerton.goncalves@unemat.br>
2019-12-11 18:04:30 -0400

    Update 11122019
    Organizando Classes

---
+++ Assets/Scripts/Agents/Agent2D_Topdown.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Agent2D_Topdown : Agent2D
+{
+    public enum AgentStates_Topdown
+    {
+        WALK,
+        IDLE
+    }
+    [Header("Agent2d Topdown Configs")]
+    public AgentStates_Topdown STATE;
+
+    protected override void UpdateStates()
+    {
+
+        if (NextWayPoint == CurrentWaypointPosition)
+        {
+
+            OnIdle();
+        }
+        else
+        {
+            OnMove();
+
+        }
+    }
+    protected override void OnChangeStates()
+    {
+        switch (STATE)
+        {
+            case AgentStates_Topdown.WALK:
+                //Executar animação correspondente aqui
+                break;
+
+            case AgentStates_Topdown.IDLE:
+                //Executar animação correspondente aqui
+                break;
+        }
+
+    }
+    protected override void OnIdle()
+    {
+        STATE = AgentStates_Topdown.IDLE;
+    }
+    protected override void OnMove()
+    {
+        STATE = AgentStates_Topdown.WALK;
+        transform.parent.right = NextWayPoint - transform.parent.position;
+        transform.parent.position = Vector3.Lerp(CurrentWaypointPosition, CurrentPath.Peek(), MoveTimeCurrent / MoveTimeTotal);
+
+
+        Debug.DrawLine(transform.parent.position, CurrentPath.Peek());//Direção
+    }
+    protected override void OnJump()
+    {
+
+    }
+    protected override void OnDrop()
+    {
+
+    }
+
+}
